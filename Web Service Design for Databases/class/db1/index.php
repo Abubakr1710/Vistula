f7e35be23875985ab5f3fce3f671a6f4b5203b5e
Abubakr Mamajonov <mamajonov1710@gmail.com>
2023-04-24 14:54:59 +0200

    task 27

---
+++ Web Service Design for Databases/class/db1/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        define("SERVERNAME", "fdb1028.awardspace.net");
+        define("USERNAME", "4297006_52493");
+        define("PASSWORD", "esELbf@brf72TEm");
+        define("DBNAME", "4297006_52493");
+        
+        // Create connection
+        $conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "SELECT id, firstname, lastname FROM MyGuests";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+        ?>
+    </body>
+</html>
