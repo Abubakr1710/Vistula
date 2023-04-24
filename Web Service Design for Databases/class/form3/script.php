2e4a88dd51d24b0cbb412e5396b43bbc70cdfa0a
Abubakr Mamajonov <mamajonov1710@gmail.com>
2023-04-24 13:14:12 +0200

    error fixed

--- Web Service Design for Databases/class/form3/script.php
+++ Web Service Design for Databases/class/form3/script.php
@@ -2,5 +2,5 @@
 $country=$_POST["country"];
 $pupulation=$_POST["population"];
 $capital=$_POST["capital"];
-echo $country.' has a capital: '.$capital.' and population: '$.population;
+echo $country.' has a capital: '.$capital.' and population: '.$population;
 ?>
